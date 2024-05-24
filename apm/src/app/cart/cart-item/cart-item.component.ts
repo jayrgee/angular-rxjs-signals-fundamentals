c08c05f71123a62072377e88bbaf82fc2c051fe6
jayrgee <jayrgee@fastmail.fm>
2024-05-24 14:44:53 +1000

    use product.quantityInStock

--- apm/src/app/cart/cart-item/cart-item.component.ts
+++ apm/src/app/cart/cart-item/cart-item.component.ts
@@ -21,9 +21,8 @@ export class CartItemComponent {
 
   item = signal<CartItem>(undefined!);
 
-  // Quantity available (hard-coded to 8)
-  // Mapped to an array from 1-8
-  qtyArr = [...Array(8).keys()].map(x => x + 1);
+  qtyArr = computed<Number[]>(() =>
+    [...Array(this.item().product.quantityInStock).keys()].map(x => x + 1));
 
   // Calculate the extended price
   // exPrice = this.cartItem?.quantity * this.cartItem?.product.price;
